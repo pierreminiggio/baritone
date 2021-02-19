661fe43c573884db657d173351c33938ef21f923
Pierre Miniggio <35456349+pierreminiggio@users.noreply.github.com>
2021-02-19 14:11:26 +0100

    Update index.php

--- index.php
+++ index.php
@@ -6,7 +6,7 @@
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Apprendre PHP</title>
+    <title>Baritone Obsessed</title>
 </head>
 <body>
     <a href="<?php echo $link ?>">Click here to go to the group.</a>
